d826ac57cec1c43a4acd7feaebe1183346f19a9d
eltchi <elsa.tchissambo@gmail.com>
2025-10-02 14:59:08 +0200

    add services cards

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
